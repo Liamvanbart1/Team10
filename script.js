a4aba787d82335379fa4c1d15b7e7600e91593ee
StijnCSS <stijnzwaard@gmail.com>
2025-03-27 11:44:24 +0100

    Merge met de boys

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const fetchDate = async () => {
         const women = data.data;
         const imageBaseURL = "https://fdnd.directus.app/assets/";
         const lists = document.querySelectorAll(".horizontal-scroll");
-
+        const popoverContainer = document.getElementById("popover-container")
+        
         const sections = {
             "way back": lists[0],
             "web pioneer": lists[1],
@@ -31,7 +32,7 @@ const fetchDate = async () => {
             // Create the button to trigger the popover
             let button = document.createElement("button");
             button.setAttribute("popovertarget", popoverId);
-            button.innerHTML = `<img src="${imageURL}" alt="${women.name}">`;
+                        button.innerHTML = `<img src="${imageURL}" alt="${women.name}" class="PillarImage">`;
 
             // Create the popover
             let popover = document.createElement("div");
@@ -40,10 +41,14 @@ const fetchDate = async () => {
             popover.classList.add("InformationPopOver");
             popover.innerHTML = `
                 <img src="${imageURL}" alt="${women.name}">
-                <div>${women.period}</div>
-                <p>${women.country_code}</p>
+                <div class="popover-text">
                 <h2>${women.name}</h2>
+                <div class="popover-tags">
+                    <p class="popover-tag-styling">${women.period}</p>
+                    <p class="popover-tag-styling">${women.country_code}</p>
+                </div>
                 <p>${women.tagline}</p>
+                </div>
             `;
 
             if (sections[women.period]) {
@@ -56,7 +61,7 @@ const fetchDate = async () => {
                 sectionCounters[women.period]++;
             }
 
-            document.body.appendChild(popover);
+            popoverContainer.appendChild(popover);
         });
 
 
@@ -71,10 +76,10 @@ fetchDate();
 
 
 
-let audio = document.getElementById("scrollSound");
+// let audio = document.getElementById("scrollSound");
 
-window.addEventListener("scroll", () => {
-  if (audio.paused) {
-    audio.play();
-  }
-});
+// window.addEventListener("scroll", () => {
+//   if (audio.paused) {
+//     audio.play();
+//   }
+// });
